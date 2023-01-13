e4310684a41b382da2b3b1a72fad14059e0e5d93
yuzo <yuzokamoto@gmail.com>
2023-01-13 16:03:08 -0300

    update

--- database/pratica-aprofundamento-sql.sql
+++ database/pratica-aprofundamento-sql.sql
@@ -7,6 +7,7 @@ DROP TABLE pokemons;
 CREATE TABLE pokemons (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     name TEXT UNIQUE NOT NULL,
+    type TEXT NOT NULL,
     hp INTEGER NOT NULL,
     attack INTEGER NOT NULL,
     defense INTEGER NOT NULL,
@@ -19,6 +20,7 @@ CREATE TABLE pokemons (
 INSERT INTO pokemons (
     id,
     name,
+    type,
     hp,
     attack,
     defense,
@@ -27,15 +29,15 @@ INSERT INTO pokemons (
     speed
 )
 VALUES 
-    (1, "bulbasaur", 45, 49, 49, 65, 65, 45),
-    (2, "ivysaur", 60, 62, 63, 80, 80, 60),
-    (3, "venusaur", 80, 82, 83, 100, 100, 80),
-    (4, "charmander", 39, 52, 43, 60, 50, 65),
-    (5, "charmeleon", 58, 64, 58, 80, 65, 80),
-    (6, "charizard", 78, 84, 78, 109, 85, 100),
-    (7, "squirtle", 44, 48, 65, 50, 64, 43),
-    (8, "wartortle", 59, 63, 80, 65, 80, 58),
-    (9, "blastoise", 79, 83, 100, 85, 105, 78);
+    (1, "bulbasaur", "grass", 45, 49, 49, 65, 65, 45),
+    (2, "ivysaur", "grass", 60, 62, 63, 80, 80, 60),
+    (3, "venusaur", "grass", 80, 82, 83, 100, 100, 80),
+    (4, "charmander", "fire", 39, 52, 43, 60, 50, 65),
+    (5, "charmeleon", "fire", 58, 64, 58, 80, 65, 80),
+    (6, "charizard", "fire", 78, 84, 78, 109, 85, 100),
+    (7, "squirtle", "water", 44, 48, 65, 50, 64, 43),
+    (8, "wartortle", "water", 59, 63, 80, 65, 80, 58),
+    (9, "blastoise", "water", 79, 83, 100, 85, 105, 78);
 
 -- Buscar todos os pokemons
 SELECT * FROM pokemons;
